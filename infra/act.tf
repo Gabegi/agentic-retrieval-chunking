dca1673a171aa9bfa366f1d7101ce8c1e68c3c85
You have one new message <george.trombley6000@gmail.com>
2026-06-15 11:10:07 +0200

    Update act.tf

--- infra/act.tf
+++ infra/act.tf
@@ -1,8 +1,3 @@
-data "azurerm_virtual_machine" "runner" {
-  name                = "vm-github-runner-lz-ai"
-  resource_group_name = "rg-github-runner"
-}
-
 resource "azurerm_container_registry" "main" {
   name                = "chuningtestacr"
   resource_group_name = azurerm_resource_group.main.name
@@ -20,12 +15,12 @@ resource "azurerm_container_registry" "main" {
 resource "azurerm_role_assignment" "runner_acr_push" {
   scope                = azurerm_container_registry.main.id
   role_definition_name = "AcrPush"
-  principal_id         = data.azurerm_virtual_machine.runner.identity[0].principal_id
+  principal_id         = data.azurerm_client_config.current.object_id
 }
 
 # Allow runner VM to upload scraped PDFs to blob storage
 resource "azurerm_role_assignment" "runner_blob_contributor" {
   scope                = azurerm_storage_account.documents.id
   role_definition_name = "Storage Blob Data Contributor"
-  principal_id         = data.azurerm_virtual_machine.runner.identity[0].principal_id
+  principal_id         = data.azurerm_client_config.current.object_id
 }
