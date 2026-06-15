fb94638e701a0a87e832893a2cf73ac713017bba
You have one new message <george.trombley6000@gmail.com>
2026-06-15 11:14:43 +0200

    Update act.tf

--- infra/act.tf
+++ infra/act.tf
@@ -17,10 +17,3 @@ resource "azurerm_role_assignment" "runner_acr_push" {
   role_definition_name = "AcrPush"
   principal_id         = data.azurerm_client_config.current.object_id
 }
-
-# Allow runner VM to upload scraped PDFs to blob storage
-resource "azurerm_role_assignment" "runner_blob_contributor" {
-  scope                = azurerm_storage_account.documents.id
-  role_definition_name = "Storage Blob Data Contributor"
-  principal_id         = data.azurerm_client_config.current.object_id
-}
