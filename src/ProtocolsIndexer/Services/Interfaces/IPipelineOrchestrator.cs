aed797cc359c16be8e840b80f06c199993a2003d
You have one new message <george.trombley6000@gmail.com>
2026-06-16 11:42:36 +0200

    Update IPipelineOrchestrator.cs

--- src/ProtocolsIndexer/Services/Interfaces/IPipelineOrchestrator.cs
+++ src/ProtocolsIndexer/Services/Interfaces/IPipelineOrchestrator.cs
@@ -3,4 +3,5 @@ namespace ProtocolsIndexer.Services;
 public interface IPipelineOrchestrator
 {
     Task RunAsync(CancellationToken ct = default);
+    Task CompareAsync(CancellationToken ct = default);
 }
