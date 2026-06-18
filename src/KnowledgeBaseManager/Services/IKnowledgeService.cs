907ef781597a30dd2ddc2e47fa83c6607cb5f707
You have one new message <george.trombley6000@gmail.com>
2026-06-18 16:39:39 +0200

    removing

--- src/KnowledgeBaseManager/Services/IKnowledgeService.cs
+++
@@ -1,7 +0,0 @@
-namespace KnowledgeBaseManager.Services;
-
-public interface IKnowledgeService
-{
-    Task EnsureKnowledgeSourceAsync(CancellationToken ct = default);
-    Task EnsureKnowledgeBaseAsync(CancellationToken ct = default);
-}
