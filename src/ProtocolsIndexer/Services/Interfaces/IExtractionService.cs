f7bd9bee1d7a473e81903fa379e558dc7a78ee3f
You have one new message <george.trombley6000@gmail.com>
2026-06-18 11:27:03 +0200

    Update IExtractionService.cs

--- src/ProtocolsIndexer/Services/Interfaces/IExtractionService.cs
+++ src/ProtocolsIndexer/Services/Interfaces/IExtractionService.cs
@@ -1,4 +1,3 @@
-using Azure.Storage.Blobs.Models;
 using ProtocolsIndexer.Models;
 
 namespace ProtocolsIndexer.Services;
@@ -6,5 +5,5 @@ namespace ProtocolsIndexer.Services;
 public interface IExtractionService
 {
     string Name { get; }
-    Task<ExtractionRun> ExtractAsync(BlobItem blob, byte[] pdfBytes, CancellationToken ct = default);
+    Task<ExtractionRun> ExtractAsync(string blobName, byte[] pdfBytes, CancellationToken ct = default);
 }
