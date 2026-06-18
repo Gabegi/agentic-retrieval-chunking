fbe2b41ed9437edb059eb6dea086c35616b42076
You have one new message <george.trombley6000@gmail.com>
2026-06-18 11:27:11 +0200

    Update IPipelineOrchestrator.cs

--- src/ProtocolsIndexer/Services/Interfaces/IPipelineOrchestrator.cs
+++ src/ProtocolsIndexer/Services/Interfaces/IPipelineOrchestrator.cs
@@ -2,6 +2,7 @@ namespace ProtocolsIndexer.Services;
 
 public interface IPipelineOrchestrator
 {
+    Task ProcessBlobAsync(string blobName, byte[] bytes, CancellationToken ct = default);
     Task RunAsync(CancellationToken ct = default);
     Task CompareAsync(CancellationToken ct = default);
 }
