df14dc936fe099b8c8bf7bb8c93fe4ed2a0f4ca1
You have one new message <george.trombley6000@gmail.com>
2026-06-23 15:10:51 +0200

    validation

---
+++ src/ProtocolsIndexer/Services/ProtocolIndexing/CSVExtraction/Models/ValidationReport.cs
@@ -0,0 +1,18 @@
+namespace ProtocolsIndexer.Models;
+
+public class ValidationReport
+{
+    public DateTime RunAtUtc              { get; init; }
+    public int      PagesExtracted        { get; init; }
+    public int      IndexRecordsExtracted { get; init; }
+    public int      JoinedRecords         { get; init; }
+    public int      CleanedRecords        { get; init; }
+    public bool     Passed                { get; init; }
+
+    public IReadOnlyList<ValidationIssue>   Issues                        { get; init; } = [];
+    public IReadOnlyList<string>            ReconciliationProblems        { get; init; } = [];
+    public IReadOnlyList<string>            MagnitudeWarnings             { get; init; } = [];
+    public IReadOnlyList<string>            RedFlags                      { get; init; } = [];
+    public IReadOnlyList<CleanedPageRecord> SpotCheckSample               { get; init; } = [];
+    public IReadOnlyList<string>            DocumentsNeedingFallbackChunking { get; init; } = [];
+}
