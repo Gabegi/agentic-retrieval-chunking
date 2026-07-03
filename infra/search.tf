746408f5cbeb601aafd53fde40fe03a2cc3387ff
You have one new message <george.trombley6000@gmail.com>
2026-07-03 13:38:14 +0200

    Update search.tf

--- infra/search.tf
+++ infra/search.tf
@@ -14,10 +14,11 @@ resource "azurerm_search_service" "main" {
 }
 
 resource "azurerm_private_endpoint" "search" {
-  name                = "cor-pep-srch-cap-${local.env}-${local.region}-${local.instance}"
-  location            = var.location
-  resource_group_name = data.azurerm_resource_group.data.name
-  subnet_id           = data.azurerm_subnet.pe.id
+  name                          = "cor-pep-srch-cap-${local.env}-${local.region}-${local.instance}"
+  location                      = var.location
+  resource_group_name           = data.azurerm_resource_group.data.name
+  subnet_id                     = data.azurerm_subnet.pe.id
+  custom_network_interface_name = "cor-pep-srch-cap-${local.env}-${local.region}-${local.instance}_nic"
 
   private_service_connection {
     name                           = "cor-pep-srch-cap-${local.env}-${local.region}-${local.instance}-psc"
