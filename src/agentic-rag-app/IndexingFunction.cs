0ba2fa837e5336d6d8d4cd4a74214d9c26cb211d
You have one new message <george.trombley6000@gmail.com>
2026-07-03 15:12:54 +0200

    Update IndexingFunction.cs

--- src/agentic-rag-app/IndexingFunction.cs
+++ src/agentic-rag-app/IndexingFunction.cs
@@ -86,7 +86,7 @@ public class IndexingFunction
         {
             extractResults = await context.CallActivityAsync<ExtractionResults>("ExtractActivity",        new ExtractRequest(input.ForceReindex, docsBlob));
             chunkResults   = await context.CallActivityAsync<ChunkingResults>("ChunkActivity",               new ChunkRequest(docsBlob, chunksBlob));
-            embedResults   = await context.CallActivityAsync<EmbedUploadingResults>("EmbedAndUploadActivity", chunksBlob);
+            embedResults   = await context.CallActivityAsync<EmbedUploadingResults>("EmbedAndUploadActivity", new EmbedUploadRequest(chunksBlob, extractResults.StaleDocumentIds));
             success      = true;
         }
         catch (Exception ex)
