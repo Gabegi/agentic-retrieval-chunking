db5ff70eaed8d826032705834a9198d64ab4214b
You have one new message <george.trombley6000@gmail.com>
2026-07-07 16:11:23 +0200

    Update storage.tf

--- infra/storage.tf
+++ infra/storage.tf
@@ -147,10 +147,10 @@ resource "azurerm_private_endpoint" "stfunc_table" {
     is_manual_connection           = false
   }
 
-  # No private_dns_zone_group here yet: privatelink.table.core.windows.net
-  # doesn't exist in the hub at all (cor-connectivity-dns-prd-we-001) - the
-  # platform team needs to create the zone first, not just link/attach it
-  # (docs/platform-team-dns-verzoek.md).
+  private_dns_zone_group {
+    name                 = "default"
+    private_dns_zone_ids = [data.azurerm_private_dns_zone.table.id]
+  }
 
   tags = local.common_tags
 }
