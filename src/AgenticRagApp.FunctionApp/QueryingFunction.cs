e87d627bdcbceb56a8adac18e8288a1bd24b4fa5
You have one new message <george.trombley6000@gmail.com>
2026-07-21 16:46:40 +0200

    Update QueryingFunction.cs

--- src/AgenticRagApp.FunctionApp/QueryingFunction.cs
+++ src/AgenticRagApp.FunctionApp/QueryingFunction.cs
@@ -4,7 +4,7 @@ using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
 using AgenticRagApp.Observability.Reports;
-using AgenticRagApp.Services;
+using AgenticRagApp.Querying.Services;
 
 namespace AgenticRagApp;
 
